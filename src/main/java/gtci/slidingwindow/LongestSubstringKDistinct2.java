ed6e9cb1fe717fbf510227796dfc50466cf75f48
Rocky Madden <git@rockymadden.com>
2020-12-20 11:18:34 -0700

    feat(LongestSubstringKDistinct2): solve with Java

---
+++ src/main/java/gtci/slidingwindow/LongestSubstringKDistinct2.java
@@ -0,0 +1,33 @@
+package gtci.slidingwindow;
+
+import java.util.*;
+
+/**
+ * Given a string, find the length of the longest substring in it with no more
+ * than K distinct characters.
+ */
+public class LongestSubstringKDistinct2 {
+    public static int findLength(String str, int k) {
+        Map<Character, Integer> freq = new HashMap<>();
+        int n = str.length();
+        int longest = 0;
+
+        for (int end = 0, start = 0, sum = 0; end < n; end++) {
+            char endChar = str.charAt(end);
+            freq.put(endChar, freq.getOrDefault(endChar, 0) + 1);
+            sum++;
+
+            while (freq.size() > k) {
+                char startChar = str.charAt(start);
+                freq.put(startChar, freq.get(startChar) - 1);
+                if (freq.get(startChar) == 0) freq.remove(startChar);
+                start++;
+                sum--;
+            }
+
+            longest = Math.max(longest, sum);
+        }
+
+        return longest;
+    }
+}
---
+++ src/test/java/gtci/slidingwindow/LongestSubstringKDistinct2Test.java
@@ -0,0 +1,34 @@
+package gtci.slidingwindow;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static gtci.slidingwindow.LongestSubstringKDistinct2.*;
+
+public class LongestSubstringKDistinct2Test {
+    @Test
+    public void example1() {
+        String str = "araaci";
+        int k = 2;
+        int actual = findLength(str, k);
+
+        assertEquals(4, actual);
+    }
+
+    @Test
+    public void example2() {
+        String str = "araaci";
+        int k = 1;
+        int actual = findLength(str, k);
+
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void example3() {
+        String str = "cbbebi";
+        int k = 3;
+        int actual = findLength(str, k);
+
+        assertEquals(5, actual);
+    }
+}
