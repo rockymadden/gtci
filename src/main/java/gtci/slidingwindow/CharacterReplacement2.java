868892e8f31ac39353bcf32dbe73e597f08c3e88
Rocky Madden <git@rockymadden.com>
2020-12-30 11:24:17 -0700

    feat(CharacterReplacement2): solve with Java

---
+++ src/main/java/gtci/slidingwindow/CharacterReplacement2.java
@@ -0,0 +1,41 @@
+package gtci.slidingwindow;
+
+import java.util.*;
+
+/**
+ * Given a string with lowercase letters only, if you are allowed to replace no more than 'k'
+ * letters with any letter, find the length of the longest substring having the same letters
+ * after replacement.
+ */
+public class CharacterReplacement2 {
+    private static boolean canReplace(Map<Character, Integer> map, int k) {
+        if (map.size() > 2) return false;
+
+        boolean fnd = false;
+
+        for (int val : map.values()) fnd = val <= k || fnd;
+
+        return fnd;
+    }
+
+    public static int findLength(String str, int k) {
+        final char[] chrs = str.toCharArray();
+        final Map<Character, Integer> map = new HashMap<>();
+        int ans = 0;
+
+        for (int end = 0, start = 0; end < chrs.length; end++) {
+            final char endChr = chrs[end];
+            map.put(endChr, map.getOrDefault(endChr, 0) + 1);
+
+            while (!canReplace(map, k)) {
+                final char startChr = chrs[start++];
+                map.put(startChr, map.get(startChr) - 1);
+                if (map.get(startChr) == 0) map.remove(startChr);
+            }
+
+            ans = Math.max(ans, end - start + 1);
+        }
+
+        return ans;
+    }
+}
---
+++ src/test/java/gtci/slidingwindow/CharacterReplacement2Test.java
@@ -0,0 +1,34 @@
+package gtci.slidingwindow;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static gtci.slidingwindow.CharacterReplacement2.*;
+
+public class CharacterReplacement2Test {
+    @Test
+    public void example1() {
+        String str = "aabccbb";
+        int k = 2;
+        int actual = findLength(str, k);
+
+        assertEquals(5, actual);
+    }
+
+    @Test
+    public void example2() {
+        String str = "abbcb";
+        int k = 1;
+        int actual = findLength(str, k);
+
+        assertEquals(4, actual);
+    }
+
+    @Test
+    public void example3() {
+        String str = "abccde";
+        int k = 1;
+        int actual = findLength(str, k);
+
+        assertEquals(3, actual);
+    }
+}
