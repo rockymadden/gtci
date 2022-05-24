a1fd90c8f54dc1529a082cdb73195b9a82ddb218
Rocky Madden <git@rockymadden.com>
2022-05-23 19:33:35 -0600

    docs(PairWithTargetSumYr2022): add problem statement

--- src/main/java/gtci/twopointers/PairWithTargetSumYr2022.java
+++ src/main/java/gtci/twopointers/PairWithTargetSumYr2022.java
@@ -1,5 +1,12 @@
 package gtci.twopointers;
 
+/**
+ * Given an array of sorted numbers and a target sum, find a pair in the array whose sum is
+ * equal to the given target.
+ *
+ * Write a function to return the indices of the two numbers (i.e. the pair) such that they add up
+ * to the given target.
+ */
 public class PairWithTargetSumYr2022 {
     public static int[] search(int[] arr, int targetSum) {
         for (int left = 0, right = arr.length - 1; left < right;) {
